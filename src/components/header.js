0599f63c5c652674f10f9fb2f933bc9820e274ab
John <cortesjohnj@gmail.com>
2021-03-30 17:59:31 -0500

    Start to create the content of Header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,14 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
+import UserContext from '../context/user';
+import * as ROUTES from '../constants/routes';
+
 export default function Header() {
+  const { firebase } = useContext(FirebaseContext);
+  const { user } = useContext(UserContext);
+
+  console.log('user', user);
+
   return <p>I'm The header</p>;
 }
