0ec0c935609614d7e3e3a8f801f389fd2a290583
John <cortesjohnj@gmail.com>
2021-04-12 16:35:07 -0500

    Created post components, and started developing Header component from Post components

---
+++ src/components/post/index.js
@@ -0,0 +1,21 @@
+import { useRef } from 'react';
+import PropTypes from 'prop-types';
+import Header from './header';
+
+export default function Post({ content }) {
+  console.log('content', content);
+  return <Header username={content.username} />;
+}
+
+Post.propTypes = {
+  content: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    imageSrc: PropTypes.string.isRequired,
+    caption: PropTypes.string.isRequired,
+    docId: PropTypes.string.isRequired,
+    userLikedPhoto: PropTypes.bool.isRequired,
+    likes: PropTypes.array.isRequired,
+    comments: PropTypes.array.isRequired,
+    dateCreated: PropTypes.number.isRequired,
+  }),
+};
