e161ce49aad46bb85bede8b6ce2d1507d3a27f6e
John <cortesjohnj@gmail.com>
2021-04-19 16:40:17 -0500

    Implemented Firebase service getUserByUsername and imported in profile component

--- src/components/spinner.js
+++ src/components/spinner.js
@@ -5,13 +5,7 @@ export default function Spinner() {
     <div className="flex h-screen justify-center content-center items-center">
       <div className="flex flex-col justify-evenly">
         <p className="text-center font-bold my-4">Loading...</p>
-        <Loader
-          type="Oval"
-          color="#962FBF"
-          height={200}
-          width={200}
-          timeout={3000}
-        ></Loader>
+        <Loader type="Oval" color="#962FBF" height={200} width={200}></Loader>
       </div>
     </div>
   );
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,3 +1,19 @@
+import { useParams, useHistory } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { doesUsernameExist } from '../services/firebase';
+
 export default function Profile() {
-  return null;
+  const { username } = useParams();
+  const [userExists, setUserExists] = useState(false);
+
+  useEffect(() => {
+    document.title = `Instagram - Profile`;
+  }, []);
+
+  useEffect(() => {
+    async function checkUserExists() {
+      const doesUserExists = await doesUsernameExist(username);
+    }
+  }, []);
+  return <p>Hello!</p>;
 }
